6c7f14f98556c1a33a7b7b6e6a6db83f4959d977
Mohd Kamran <106367617+Mohd-Kamran-rethink@users.noreply.github.com>
2023-06-09 18:43:53 +0530

    databasechanegs

--- database/migrations/2023_06_08_132758_expenses.php
+++ database/migrations/2023_06_08_132758_expenses.php
@@ -16,16 +16,15 @@ class Expenses extends Migration
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
             $table->string('user_id');
-            $table->enum('type',['Debit','Credit']);
             $table->string('department_id');
-            $table->string('debiter_id');
+            $table->string('creditor_id')->nullable();
             $table->string('expense_type_id');
             $table->enum('transaction_type',['Cash','Bank']);
             $table->string('bank_id');
             $table->string('currency_type');
             $table->string('amount');
-            $table->string('remark');
-            $table->string('attatchement');
+            $table->string('remark')->nullable();
+            $table->string('attatchement')->nullable();
             $table->timestamps();
         });
     }
