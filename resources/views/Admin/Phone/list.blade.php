f650bf49e644ba4e78806535a16c5a6325befeb2
Mohd Kamran <106367617+Mohd-Kamran-rethink@users.noreply.github.com>
2023-07-06 19:25:05 +0530

    chanhes

--- resources/views/Admin/Phone/list.blade.php
+++ resources/views/Admin/Phone/list.blade.php
@@ -1,7 +1,7 @@
 @extends('Admin.index')
 @section('content')
-<?php $platforms = array('wa'=>'Whatsapp','wa_c'=>'Whatsapp Clone','wa_b'=>'Whatsapp Business');?>
-<?php $devices = array('galaxy_a04'=>'Galaxy A04','galaxy_a03_core'=>'Galaxy A03 Core','redmi_9a'=>'Redmi 9A','oppo_f1s'=>'Oppo F1S','galaxy_a03'=>'Galaxy A03','galaxy_s22'=>'Galaxy S22');?>
+    <?php $platforms = ['wa' => 'Whatsapp', 'wa_c' => 'Whatsapp Clone', 'wa_b' => 'Whatsapp Business']; ?>
+    <?php $devices = ['galaxy_a04' => 'Galaxy A04', 'galaxy_a03_core' => 'Galaxy A03 Core', 'redmi_9a' => 'Redmi 9A', 'oppo_f1s' => 'Oppo F1S', 'galaxy_a03' => 'Galaxy A03', 'galaxy_s22' => 'Galaxy S22']; ?>
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
@@ -27,31 +27,49 @@
         <div class="card">
             <div class="card-body">
                 <div class="mb-3 d-flex justify-content-between align-items-centers">
-            <form class="filters d-flex flex-row  col-lg-7 mt-2" action="{{ url('leads/list') }}" method="GET"
+                    <form class="filters d-flex flex-row  col-lg-7 mt-2" action="{{ url('leads/list') }}" method="GET"
                         id="search-form">
                         <div class="input-group input-group-md col-3 " style="width: 150px;">
                             <input type="text" value="{{ isset($searchTerm) ? $searchTerm : '' }}" name="table_search"
                                 class="form-control float-right" placeholder="Search" id="searchInput">
                         </div>
                         <div class="input-group col-3">
-                            <select name="platform_seach" id="platform_seach"  class="form-control">
+                            <select name="platform_seach" id="platform_seach" class="form-control">
                                 <option value="">--Filter By Platform--</option>
-                                <option {{isset($platform_search) && $platform_search =='wati'?'selected':''}} value="wati">Wati</option>
-                                <option {{isset($platform_search) && $platform_search =='wa'?'selected':''}} value="wa">Whatsapp</option>
-                                <option {{isset($platform_search) && $platform_search =='wa_b'?'selected':''}} value="wa_b">Whatsapp Business</option>
-                                <option {{isset($platform_search) && $platform_search =='wa_c'?'selected':''}} value="wa_c">Whatsapp Clone</option>
+                                <option {{ isset($platform_search) && $platform_search == 'wati' ? 'selected' : '' }}
+                                    value="wati">Wati</option>
+                                <option {{ isset($platform_search) && $platform_search == 'wa' ? 'selected' : '' }}
+                                    value="wa">Whatsapp</option>
+                                <option {{ isset($platform_search) && $platform_search == 'wa_b' ? 'selected' : '' }}
+                                    value="wa_b">Whatsapp Business</option>
+                                <option {{ isset($platform_search) && $platform_search == 'wa_c' ? 'selected' : '' }}
+                                    value="wa_c">Whatsapp Clone</option>
                             </select>
                         </div>
                         <div class="input-group col-3">
-                                <select name="status" id="status_search" class="form-control">
-                                    <option value="">--Filter By Status--</option>
-                                    <option {{isset($status) && $status=="review"?'selected':''}} value="review">Review</option>
-                                    <option {{isset($status) && $status=="otp"?'selected':''}} value="otp">OTP Needed</option>
-                                    <option {{isset($status) && $status=="active"?'selected':''}} value="active">Assigned</option>
-                                    <option {{isset($status) && $status=="inactive"?'selected':''}} value="inactive">In Active</option>
-                                    <option {{isset($status) && $status=="banned"?'selected':''}} value="banned">Banned</option>
-                                </select>
-                            </div>
+                            <select name="status" id="status_search" class="form-control">
+                                <option value="">--Filter By Status--</option>
+                                <option {{ isset($status) && $status == 'review' ? 'selected' : '' }} value="review">Review
+                                </option>
+                                <option {{ isset($status) && $status == 'otp' ? 'selected' : '' }} value="otp">OTP Needed
+                                </option>
+                                <option {{ isset($status) && $status == 'active' ? 'selected' : '' }} value="active">Assigned
+                                </option>
+                                <option {{ isset($status) && $status == 'inactive' ? 'selected' : '' }} value="inactive">In
+                                    Active</option>
+                                <option {{ isset($status) && $status == 'banned' ? 'selected' : '' }} value="banned">Banned
+                                </option>
+                            </select>
+                        </div>
+                        <div class="input-group col-3">
+                            <select name="filetByAgent" id="" class="form-control">
+                                <option value="">--Filer by Agent--</option>
+                                @foreach ($agents as $item)
+                                    <option value="{{$item->id}}">{{$item->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                                    
                         <div class="input-group ">
                             <button class="btn btn-success" onclick="searchData()">Filter</button>
                         </div>
@@ -81,42 +99,44 @@
                                         @forelse ($numbers as $item)
                                             <tr>
                                                 <td>{{ $loop->iteration }}</td>
-                                                <td>{{ $item->number??'' }}</td>
+                                                <td>{{ $item->number ?? '' }}</td>
                                                 <td>
-                                                    @if($item->platform=='wa')
+                                                    @if ($item->platform == 'wa')
                                                         Whatsapp
-                                                       @elseif($item->platform=='wa_b')
-                                                       Whatsapp Business
-                                                       @elseif($item->platform=='wa_c')
-                                                       Whatsapp Clone
-                                                       @elseif($item->platform=='wati')
-                                                       Wati
+                                                    @elseif($item->platform == 'wa_b')
+                                                        Whatsapp Business
+                                                    @elseif($item->platform == 'wa_c')
+                                                        Whatsapp Clone
+                                                    @elseif($item->platform == 'wati')
+                                                        Wati
                                                     @endif
 
                                                 </td>
-                                                <td>{{ $item->PhoneAgent??'Spare' }}</td>
-                                                <td>{{ $devices[$item->device_name]??'' }}</td>
+                                                <td>{{ $item->PhoneAgent ?? 'Spare' }}</td>
+                                                <td>{{ $item->device_name ?? '' }}</td>
                                                 <td>{{ $item->device_code }}</td>
-                                                <td style="text-transform: capitalize">{{$item->status=='active'&&$item->PhoneAgent?'Assigned': $item->status }}</td>
+                                                <td style="text-transform: capitalize">
+                                                    {{ $item->status == 'active' && $item->PhoneAgent ? 'Assigned' : $item->status }}
+                                                </td>
                                                 <td>
-                                                    
-                                                    <a href="{{ url('phone-numbers/edit?id='.$item->id) }}"
+
+                                                    <a href="{{ url('phone-numbers/edit?id=' . $item->id) }}"
                                                         title="Edit this source" class="btn btn-primary"><i
                                                             class="fa fa-pen"></i></a>
-                                                            <button title="Change Status"
-                                                            onclick="reassignModal({{$item->id}})"
-                                                            class="btn btn-danger">Reassign</button>
+                                                    <button title="Change Status"
+                                                        onclick="reassignModal({{ $item->id }})"
+                                                        class="btn btn-danger">Reassign</button>
                                                     <button title="Change Status"
                                                         onclick="manageModal({{ $item->id }})"
                                                         class="btn btn-danger">Change Status</button>
-                                                        <a href="{{ url('phone-numbers/history?id='.$item->id) }}"
-                                                            title="History" class="btn btn-warning">History</a>
-                                                            
+                                                    <a href="{{ url('phone-numbers/history?id=' . $item->id) }}"
+                                                        title="History" class="btn btn-warning">History</a>
+
                                                 </td>
 
 
                                             </tr>
-                                            @empty
+                                        @empty
                                             <tr>
                                                 <td colspan="10" class="text-center">No data</td>
                                             </tr>
@@ -145,28 +165,29 @@
                     <form action="{{ url('/phone-numbers/reassign') }}" method="POST">
                         @csrf
                         <input type="hidden" name="id" id="reasignID">
-                            <div class="form-group mx-3">
-                                <label for="">Agent</label>
-                                <select style="width: 100%" class="form-control searchOptions" name="agent" id="status">
-                                    <option value="0">--Choose--</option>
-                                    <option value="">SPARE</option>
-                                    @foreach ($agents as $item)
-                                    <option value="{{$item->id}}">{{$item->name}}</option>
-                                    @endforeach
-                                </select>
-                            </div><!-- 
-                            <div class="form-group mx-3">
-                                <label for="">Platform</label>
-                                <select class="form-control" name="platform" id="status">
-                                    <option value="0">--Choose--</option>
-                                    <option value="whatsapp">WhatsApp</option>
-                                    <option value="whatsapp_b">WhatsApp Business</option>
-                                    <option value="wati_b">Whatsapp Clone</option>
-                                    <option value="wati">WATI</option>
-                                  
-                                </select>
-                            </div> -->
-                                    
+                        <div class="form-group mx-3">
+                            <label for="">Agent</label>
+                            <select style="width: 100%" class="form-control searchOptions" name="agent" id="status">
+                                <option value="0">--Choose--</option>
+                                <option value="">SPARE</option>
+                                @foreach ($agents as $item)
+                                    <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <!--
+                                <div class="form-group mx-3">
+                                    <label for="">Platform</label>
+                                    <select class="form-control" name="platform" id="status">
+                                        <option value="0">--Choose--</option>
+                                        <option value="whatsapp">WhatsApp</option>
+                                        <option value="whatsapp_b">WhatsApp Business</option>
+                                        <option value="wati_b">Whatsapp Clone</option>
+                                        <option value="wati">WATI</option>
+                                      
+                                    </select>
+                                </div> -->
+
                         <div class="modal-footer ">
                             <button type="submit" class="btn btn-danger">Submit</button>
                             <button type="button" data-dismiss="modal" aria-label="Close"
@@ -188,17 +209,17 @@
                 <form action="{{ url('/phone-numbers/change-status') }}" method="POST">
                     @csrf
                     <input type="hidden" name="id" id="deleteInput">
-                        <div class="form-group mx-3">
-                            <label for="">Select</label>
-                            <select class="form-control" name="status" id="status">
-                                <option value="0">--Choose--</option>
-                                <option value="review">Review</option>
-                                <option value="otp">OTP Needed</option>
-                                <option value="assigned">Assigned</option>
-                                <option value="inactive">In Active</option>
-                                <option value="banned">Banned</option>
-                            </select>
-                        </div>
+                    <div class="form-group mx-3">
+                        <label for="">Select</label>
+                        <select class="form-control" name="status" id="status">
+                            <option value="0">--Choose--</option>
+                            <option value="review">Review</option>
+                            <option value="otp">OTP Needed</option>
+                            <option value="assigned">Assigned</option>
+                            <option value="inactive">In Active</option>
+                            <option value="banned">Banned</option>
+                        </select>
+                    </div>
                     <div class="modal-footer ">
                         <button type="submit" class="btn btn-danger">Submit</button>
                         <button type="button" data-dismiss="modal" aria-label="Close"
@@ -207,7 +228,7 @@
             </div>
         </div>
     </div>
-    
+
     <script>
         const searchData = () => {
             event.preventDefault();
@@ -221,8 +242,8 @@
             url.searchParams.set('agent_id_search', searchValue);
             $('#search-form').attr('action', url.toString()).submit();
         }
-        function reassignModal(id)
-        {
+
+        function reassignModal(id) {
             console.log(id);
             $('#reassignModal').modal('show')
             $('#reasignID').val(id)
